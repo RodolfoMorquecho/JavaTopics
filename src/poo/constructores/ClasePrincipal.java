a1439f759bf19c432becaf193aad58a7e93ae77f
RodolfoMorquecho <launchx07393@innovaccion.mx>
2022-12-16 23:54:52 -0600

    Nuevos Cambios

---
+++ src/poo/constructores/ClasePrincipal.java
@@ -0,0 +1,7 @@
+package poo.constructores;
+
+public class ClasePrincipal {
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo :D");
+    }
+}
--- src/poo/thread_sincronizacion/Hilo4.java
+++ src/poo/thread_sincronizacion/Hilo4.java
@@ -3,6 +3,6 @@ package poo.thread_sincronizacion;
 public class Hilo4 implements Runnable {
     @Override
     public void run() {
-        
+
     }
 }
