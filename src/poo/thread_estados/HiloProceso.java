f3ffe1a6a1bb126afdc347528f76a86d85560f26
RodolfoMorquecho <launchx07393@innovaccion.mx>
2022-12-16 23:33:15 -0600

    Nuevos Cambios

--- src/poo/thread_estados/HiloProceso.java
+++ src/poo/thread_estados/HiloProceso.java
@@ -2,14 +2,21 @@ package poo.thread_estados;
 
 public class HiloProceso implements Runnable{
     String nombre;
-    public HiloProceso(String NombreHilo){
+    Thread t;  //Variable de tipo "Thread" para crear un hilo que corte el proceso interno del for por un segundo
+
+    public HiloProceso(String NombreHilo){  //Constructor para pasar nombre de Hilo
         this.nombre = NombreHilo;
     }
     @Override
     public void run(){
         for (int i=0;i<=5;i++){
             System.out.println(i +" "+ nombre);
+
+            try{
+                t.sleep(1000);  //Se dentra un segundo entre cada numero
+            }catch (InterruptedException e){
+                System.out.println("Error!"+e);
+            }
         }
-        System.out.println("");
     }
 }
