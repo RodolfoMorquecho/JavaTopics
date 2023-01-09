7b606f64ace0795cb54df3c06580b70aa2d3eec8
RodolfoMorquecho <launchx07393@innovaccion.mx>
2023-01-09 02:40:39 -0600

    Nuevos Cambios

---
+++ src/interfaz_grafica/graphics/grafica_circular/GraficaCircular.java
@@ -0,0 +1,13 @@
+package interfaz_grafica.graphics.grafica_circular;
+
+import javax.swing.*;
+
+public class GraficaCircular {
+    private JPanel panel1;
+    private JLabel lblTitulo;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JButton graficarButton;
+    private JLabel punto;
+}
