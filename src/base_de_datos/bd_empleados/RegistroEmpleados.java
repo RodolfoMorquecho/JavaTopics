aaa0f951c1178e63958fb4f1bd4a0514c3cecbbf
RodolfoMorquecho <launchx07393@innovaccion.mx>
2023-01-23 23:13:33 -0600

    Nuevos Cambios

---
+++ src/base_de_datos/bd_empleados/RegistroEmpleados.java
@@ -0,0 +1,14 @@
+package base_de_datos.bd_empleados;
+
+import javax.swing.*;
+
+public class RegistroEmpleados {
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JButton registrarButton;
+    private JButton modificarButton;
+    private JButton eliminarButton;
+    private JTextField textField4;
+    private JButton buscarButton;
+}
