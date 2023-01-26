1b7bff7d5482838eb647a07809e41237524c4c3b
RodolfoMorquecho <launchx07393@innovaccion.mx>
2023-01-25 20:11:55 -0600

    Nuevos Cambios

--- src/base_de_datos/bd_empleados/RegistroEmpleados.java
+++ src/base_de_datos/bd_empleados/RegistroEmpleados.java
@@ -1,5 +1,7 @@
 package base_de_datos.bd_empleados;
 
+//Importante modificar "toString" en los items, en caso de existir error
+
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -15,7 +17,7 @@ public class RegistroEmpleados extends JFrame{
     private JButton btnModificar;
     private JButton btnEliminar;
     private JTextField txt_buscar;
-    private JButton btnBustar;
+    private JButton btnBuscar;
     private JComboBox txt_entrada;
     private JComboBox txt_salida;
     private JPanel panel1;
@@ -58,8 +60,8 @@ public class RegistroEmpleados extends JFrame{
             }
         });
 
-        //Modificar
-        btnModificar.addActionListener(new ActionListener() {
+        //Leer-Buscar
+        btnBuscar.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 try {
@@ -72,12 +74,60 @@ public class RegistroEmpleados extends JFrame{
 
                     //LLenar los campos con la información buscada por el usuario, mediante el objeto rs de la calse ResultSet
                     if(rs.next()){  //Si se encuentra el resultado en la tabla:
-                        txt_nombre.setText(rs.getString("NombreEmpleados"));  //nombre del campo
-                        txt_entrada.setSelectedItem(rs.getString("HoraEntrada"));
-                        txt_salida.setSelectedItem(rs.getString("HoraSalida"));
-                    }else {
+                        txt_nombre.setText(rs.getString("NombreEmp"));  //nombre del campo
+                        txt_entrada.setSelectedItem(rs.getString("HoraEnt"));
+                        txt_salida.setSelectedItem(rs.getString("HoraSal"));
+                        //label_status.setText("");
+                    }else {  //En caso de no estar el resultado en la tabla:
                         JOptionPane.showMessageDialog(null,"Emleado no registrado.");
                     }
+                }catch (Exception exception){
+
+                }
+            }
+        });
+
+        //Modificar
+        btnModificar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    String ID = txt_buscar.getText().trim();  //Se obtiene el registro dentro del campo "buscar" y se asigna a la variable ID
+
+                    Connection cn = DriverManager.getConnection("jdbc:mysql://localhost/bd_emp","root","");
+                    PreparedStatement pst = cn.prepareStatement("update empleados set NombreEmp=?,HoraEnt=?,HoraSal=? where ID = "+ID);
+
+                    pst.setString(1,txt_nombre.getText().trim());
+                    pst.setString(2,txt_entrada.getSelectedItem().toString());
+                    pst.setString(3,txt_salida.getSelectedItem().toString());
+
+                    pst.executeUpdate();  //Se ejecuta la instrucciones previas hacia la base de datos
+
+                    label_status.setText("Modificacion exitosa.");
+                }catch (Exception exception){
+
+                }
+            }
+        });
+
+        //Eliminar
+        btnEliminar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    Connection cn = DriverManager.getConnection("jdbc:mysql://localhost/bd_emp","root","");
+                    PreparedStatement pst = cn.prepareStatement("delete from empleados where ID = ?");  //Eliminar de la tabla donde ID =?
+
+                    pst.setString(1,txt_buscar.getText().trim());  //Desde el campo buscar
+
+                    pst.executeUpdate();
+
+                    //Limpiar los campos del ID eliminado
+                    txt_nombre.setText("");
+                    txt_entrada.setSelectedItem("");
+                    txt_salida.setSelectedItem("");
+
+                    label_status.setText("Registro Eliminado");
                 }catch (Exception exception){
 
                 }
