47b635cda08c915928b8423fa44110d8f14225ea
RodolfoMorquecho <launchx07393@innovaccion.mx>
2023-01-28 01:54:33 -0600

    Nuevos Cambios

--- src/base_de_datos/creacion_reportes_pdf/RegistroEstudiante.java
+++ src/base_de_datos/creacion_reportes_pdf/RegistroEstudiante.java
@@ -1,4 +1,32 @@
 package base_de_datos.creacion_reportes_pdf;
+import java.sql.*;
+import javax.swing.*;
 
-public class RegistroEstudiante {
+public class RegistroEstudiante extends JFrame{
+    private JTextField txt_nombre;
+    private JTextField txr_grupo;
+    private JButton btnRegistrar;
+    private JButton btnReportes;
+    private JButton btnEliminar;
+    private JButton btnModificar;
+    private JTextField txt_buscar;
+    private JButton btnBuscar;
+    private JPanel mainPanel;
+
+    //Constructor
+    public RegistroEstudiante(){
+        this.setContentPane(mainPanel);
+        setTitle("Generador de reportes estudiantil");  //Titulo de interfaz
+        setBounds(0,0,600,450);  //Medidas de interfaz
+        setLocationRelativeTo(null);  //Centrar interfaz
+    }
+
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new RegistroEstudiante().setVisible(true);  //mediante un objeto anonimo, accedemos al metodo setVisible y lo activamos
+            }
+        });
+    }
 }
