723925d947e32d77181c4e24a68ea9c61fe62bfe
RodolfoMorquecho <launchx07393@innovaccion.mx>
2023-01-28 23:16:32 -0600

    Nuevos Cambios

--- src/base_de_datos/creacion_reportes_pdf/RegistroEstudiante.java
+++ src/base_de_datos/creacion_reportes_pdf/RegistroEstudiante.java
@@ -2,8 +2,15 @@ package base_de_datos.creacion_reportes_pdf;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.FileOutputStream;
 import java.sql.*;
 import javax.swing.*;
+
+//Import que daran acceso a las clases que nos proporciona "ITEXT", se agregaran manualmente ya que no pertenecen al JDK e Intellij no las agregara
+import com.itextpdf.text.Document;  //La primer clase importada sera "Document"
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.pdf.PdfPTable;  //De esta forma se obtiene la clase PdfPTable
+import com.itextpdf.text.pdf.PdfWriter;
 
 public class RegistroEstudiante extends JFrame{
     private JTextField txt_nombre;
@@ -118,6 +125,39 @@ public class RegistroEstudiante extends JFrame{
 
                     //Indicación de eliminación de registro
                     label_status.setText("Registro eliminado.");
+                }catch (Exception exception){
+
+                }
+            }
+        });
+
+        //Boton para generar reportes en PDF
+        btnReportes.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //Se creara un objeto de la clase "Document"
+                Document documento = new Document();
+
+                //Al igual que cuando se crea código que conecta con base de datos, cuando se escribe código para crear archivo
+                //en PDF, es necesario colocarlo dentro de una estructura try - catch
+                try {
+                    //Lo primero es declarar una variable de tipo String llamada ruta
+
+                    //Lo que se guardara en la variable sera la ruta que nos da acceso a la carpeta de usuario de la computadora
+                    //donde se este ejecutando el programa, es decír, la ruta que aparece al abrir el simbolo de sistema de la PC
+                    //Con esta ruta podemos indicar en que parte(Escritorio, Documentos, etc.) queremos guardar los archivos PDF
+                    String ruta = System.getProperty("uset.home");
+
+                    //Crear línea de código para indicar al programa donde va a guardar el archivo y cual sera el nombre:
+
+                    //Mediante la clase "PdfWriter" accederemos al método "getInstance", el cual pide 2 parametros:
+                    //Primero: Es un objeto de la clase "Document", usaremos el que creamos al inicio de este boton "documento"
+                    //Segundo: Se hara uso de la clase "FileOutputStream" por lo que crearemos un nuevo objeto anonimo de esta y dentro
+                    //de los parentesis se colocara la variable "ruta"(donde se guardo el acceso a la carpeta de usuario) y se
+                    //va a concatenar la boblioteca donde se guardara, y el nombre del archivo junto con su extension ".pdf"
+
+                    //Se creara el archivo en el escritorio y tendra el nombre de "Reporte_Estudiantes.pdf"
+                    PdfWriter.getInstance(documento,new FileOutputStream(ruta + "/Desktop/Reporte_Estudiantes.pdf"));
                 }catch (Exception exception){
 
                 }
