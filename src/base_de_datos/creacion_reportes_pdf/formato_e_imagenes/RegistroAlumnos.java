6140c118de1e3bfafb606aa418d8d06de18b021a
RodolfoMorquecho <launchx07393@innovaccion.mx>
2023-02-01 17:14:20 -0600

    Nuevos Cambios

--- src/base_de_datos/creacion_reportes_pdf/formato_e_imagenes/RegistroAlumnos.java
+++ src/base_de_datos/creacion_reportes_pdf/formato_e_imagenes/RegistroAlumnos.java
@@ -1,4 +1,16 @@
 package base_de_datos.creacion_reportes_pdf.formato_e_imagenes;
 
+import javax.swing.*;
+
 public class RegistroAlumnos {
+    private JTextField txt_nombre;
+    private JTextField txt_grupo;
+    private JButton btnRegistrar;
+    private JButton btnModificar;
+    private JButton btnEliminar;
+    private JButton btnReportes;
+    private JTextField txt_buscar;
+    private JButton btnBuscar;
+    private JLabel label_status;
+    private JPanel mainPanel;
 }
