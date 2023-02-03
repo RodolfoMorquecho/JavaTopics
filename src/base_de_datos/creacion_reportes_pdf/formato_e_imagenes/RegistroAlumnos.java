83e9420ede8198d106c4ddd1ecf7979e26c7cada
RodolfoMorquecho <launchx07393@innovaccion.mx>
2023-02-02 19:26:52 -0600

    Nuevos Cambios

--- src/base_de_datos/creacion_reportes_pdf/formato_e_imagenes/RegistroAlumnos.java
+++ src/base_de_datos/creacion_reportes_pdf/formato_e_imagenes/RegistroAlumnos.java
@@ -1,8 +1,11 @@
 package base_de_datos.creacion_reportes_pdf.formato_e_imagenes;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.*;
 import javax.swing.*;
 
-public class RegistroAlumnos {
+public class RegistroAlumnos extends JFrame{
     private JTextField txt_nombre;
     private JTextField txt_grupo;
     private JButton btnRegistrar;
@@ -12,5 +15,135 @@ public class RegistroAlumnos {
     private JTextField txt_buscar;
     private JButton btnBuscar;
     private JLabel label_status;
-    private JPanel mainPanel;
+    private JPanel mainPanel2;
+
+    public RegistroAlumnos(){
+        setContentPane(mainPanel2);
+        setBounds(0,0,600,450);
+        setTitle("Generador de reportes");
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        //Registrar
+        btnRegistrar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    Connection cn = DriverManager.getConnection("jdbc:mysql://localhost/bd_esc","root","");
+                    PreparedStatement pst = cn.prepareStatement("insertt into alumnos values(?,?,?)");
+
+                    pst.setString(1,"0");
+                    pst.setString(2,txt_nombre.getText().trim());
+                    pst.setString(3,txt_grupo.getText().trim());
+
+                    pst.executeUpdate();
+
+                    txt_nombre.setText("");
+                    txt_grupo.setText("");
+
+                    label_status.setText("Registro exitoso.");
+                }catch (Exception exception){
+
+                }
+            }
+        });
+
+        //Buscar
+        btnBuscar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    Connection cn = DriverManager.getConnection("jdbc:mysql://localhost/bd_esc","root","");
+                    PreparedStatement pst = cn.prepareStatement("select * from alumnos where ID = ?");
+
+                    pst.setString(1,txt_buscar.getText().trim());
+
+                    ResultSet rs = pst.executeQuery();
+
+                    if (rs.next()){
+                        txt_nombre.setText(rs.getString("NombreAlumno"));
+                        txt_grupo.setText(rs.getString("Grupo"));
+                    }else {
+                        JOptionPane.showMessageDialog(null,"Estudiante no registrado.");
+                    }
+                }catch (Exception exception){
+
+                }
+            }
+        });
+
+        //Modificar
+        btnModificar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    String ID = txt_buscar.getText().trim();
+
+                    Connection cn = DriverManager.getConnection("jdbc:mysql://localhost/bd_esc","root","");
+                    PreparedStatement pst = cn.prepareStatement("update alumnos set NombreAlumno=?, Grupo=? where ID="+ID);
+
+                    pst.setString(1,txt_nombre.getText().trim());
+                    pst.setString(2,txt_grupo.getText().trim());
+
+                    pst.executeUpdate();
+
+                    txt_nombre.setText("");
+                    txt_grupo.setText("");
+                    label_status.setText("Modificación exitosa.");
+                }catch (Exception exception){
+
+                }
+            }
+        });
+
+        //Eliminar
+        btnEliminar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    Connection cn = DriverManager.getConnection("jdbc:mysql://localhost/bd_esc","root","");
+                    PreparedStatement pst = cn.prepareStatement("delete * from alumnos where ID = ?");
+
+                    pst.setString(1,txt_buscar.getText().trim());
+
+                    pst.executeUpdate();
+
+                    txt_nombre.setText("");
+                    txt_grupo.setText("");
+                    label_status.setText("Registro eliminado.");
+                }catch (Exception exception){
+
+                }
+            }
+        });
+
+        //Generar reporte
+
+        //1. Crear un objeto de tipo "Document"     ---- try - catch
+        //2. Guardar en una variable de tipo string la ruta que que da acceso a la carpeta de usuario.
+        //3. Crear el archivo en el escritorio usando la variable, concatenando la direccion final y el nombre del archivo.pdf
+        //4. Abrir el objeto de tipo "Document"
+        //5. Crear objeto de tipo "PdfPTable" al cual se le pasan como argumento el número de columnas
+        //6. Mediante el objeto de tipo Table, agregar las celdas de titulo correspondiente a cada columna    [try - catch]
+        //7. Conectar a base de datos
+        //8. Instruccion a la base de datos: "Seleccionar tod0 de la tabla alumnos"
+        //9. Crear objeto de tipo ResultSet para verificar si se encuentra el ID, asignandole la ejecución a base de datos
+        btnReportes.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+
+
+
+    public static void main(String[] args){
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new RegistroAlumnos().setVisible(true);
+            }
+        });
+    }
 }
--- src/base_de_datos/creacion_reportes_pdf/simple/README.md
+++ src/base_de_datos/creacion_reportes_pdf/simple/README.md
@@ -1,27 +1,3 @@
-# Librería "ITEXT"
-
-Es una librería de código abierto la cual nos va a permitir generar y manipular archivos en formato PDF
-a través de java.
-
-**Importante:** Aunque ITEXT es de código abierto, no significa que sea gratuito, es decír, si
-se crean proyectos para comercializar u obtener ingresos, ITEXT pide que se adquiera una licencia,
-de lo contrario, si el proyecto es gratuito o de código abierto, se puede utilizar la versión gratuita.
-
-[Link de descarga | Version itext5-5.5.12](https://mega.nz/file/lJozxCDb#siW6qarBVy8etqnBTIEkC8fCqkgsOAcu4gysK41GFNc)
-
-## Instalación y configuración de controlador(conector)
-
-Antes de comenzar el proyecto es necesario descargar el driver con la verisión itext5-5.5.12. Una vez
-descargado, realizar los siguientes pasos para instalar adecuadamente:
-- Dar doble click en el archivo de tipo WinRar que se descargo, para que nos muestre su contenido.
-- Buscar el archivo "itext5-itextpdf-5.5.12.jar" y lo arratramos fuera de la ventana.
-- Copiamos el archivo y lo pegamos en la carpeta donde esta nuestro proyecto, al igual que con driver-connector.
-- Ahora nos dirigimos a nuestro proyecto dentro de Intellij y seleccionamos la carpeta que acabamos de agregar.
-- Dar click derecho sobre ella y seleccionamos la opción "Add as a library".
-
-Ahora todo esta listo para comenzar a crear la interfaz desde java e implementar la librería.
-
-
 ## Crear reporte de los registros de estudiantes en un archivo PDF
 
 En la carpeta "BD_Alumnos" ya se ha explicado como crear, leer, modificar y eliminar un
@@ -33,12 +9,12 @@ para generar un reporte en archivo de tipo PDF. El cual funciona de la siguiente
 
 - Primero se generan los registros necesarios desde la interfaz y se almacenan en la base de datos.
 
-![](../img/1CrearRegistros.png)
+![](img/1CrearRegistros.png)
 
 - Se verifican que los datos esten escritos correctamente y en caso de que exita un error de 
 escritura, dato o valor, se realiza una modificación.
 
-![](../img/2VerificarRegistrosEnServerBD.png)
+![](img/2VerificarRegistrosEnServerBD.png)
 
 - En caso de tener algun registro que no necesitamos, simplemente se elimina.
 
@@ -47,9 +23,9 @@ el cual nos crea un archivo llamado "Reporte_Estudiantes.pdf" en el escritorio. 
 contiene todos los regitros ordenados dentro de una tabla con las 3 columnas que separan los 
 3 tipos de datos que se requieren para registrar un estudiante(ID, NombreEstudiante, Grupo).
 
-![](../img/3GenerarReporteEnEscritorio.png)
+![](img/3GenerarReporteEnEscritorio.png)
 
-![](../img/4ArchivoDeReporte.png)
+![](img/4ArchivoDeReporte.png)
 
 
 
