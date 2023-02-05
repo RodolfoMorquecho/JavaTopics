68898cfbe77cb7b98753d21452400e19f9f4e9d7
RodolfoMorquecho <launchx07393@innovaccion.mx>
2023-02-05 00:39:25 -0600

    Nuevos Cambios

--- src/base_de_datos/reporte_musical/CarpetaMusical.java
+++ src/base_de_datos/reporte_musical/CarpetaMusical.java
@@ -2,11 +2,18 @@ package base_de_datos.reporte_musical;
 
 import javax.swing.*;
 
-public class CarpetaMusical {
+public class CarpetaMusical extends JFrame{
     private JPanel panel1;
-    private JTextField textField1;
-    private JTextField textField2;
-    private JTextField textField3;
-    private JTextField textField4;
-    private JTextField textField5;
+    private JTextField txt_nombre;
+    private JTextField txt_genero;
+    private JTextField txt_pais;
+    private JTextField txt_album;
+    private JTextField txt_numAlbum;
+    private JButton btnRegistrar;
+    private JButton btnModificar;
+    private JButton btnEliminar;
+    private JButton btnReporte;
+    private JTextField txt_buscar;
+    private JButton btnBuscar;
+    private JLabel label_status;
 }
