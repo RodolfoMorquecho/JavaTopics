795243b4297c0fed1cd3b13335425dbf7001169a
RodolfoMorquecho <launchx07393@innovaccion.mx>
2023-02-10 20:40:02 -0600

    Nuevos Cambios

--- src/base_de_datos/reporte_musical/Conexion.java
+++ src/base_de_datos/reporte_musical/Conexion.java
@@ -7,10 +7,12 @@ import java.sql.SQLException;
 
 public class Conexion {
     static String url = "jdbc:mysql://localhost/bd_music";  //Se hace estatica para poder usarla dentro del método que sera estatico también
+    static String user = "root";
+    static String password = "";
 
     public static Connection conectar(){  //Para evitar instanciarlo, se crea un método de tipo static, el cual retorna una conexión
         try {
-            Connection cn = DriverManager.getConnection(url,"root","");
+            Connection cn = DriverManager.getConnection(url, user, password);
             return cn;
         }catch (SQLException exception){
             System.out.println("Error de conexión "+ exception);
