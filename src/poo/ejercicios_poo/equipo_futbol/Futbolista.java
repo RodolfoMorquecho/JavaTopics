6bf1e5dad684e586b21b035f1d5e074cba23c0ce
RodolfoMorquecho <launchx07393@innovaccion.mx>
2023-03-17 23:38:11 -0600

    Nuevos Cambios

---
+++ src/poo/ejercicios_poo/equipo_futbol/Futbolista.java
@@ -0,0 +1,42 @@
+package poo.ejercicios_poo.equipo_futbol;
+
+public class Futbolista extends Persona{
+    String posicion;
+    int dorsal;
+
+    public Futbolista(){}
+
+    public Futbolista(String nombre, String sexo, int edad, String posicion, int dorsal){
+        super(nombre, sexo, edad);
+        this.posicion = posicion;
+        this.dorsal = dorsal;
+    }
+
+    //Setters y Getters
+
+    public String getPosicion(){
+        return posicion;
+    }
+
+    public boolean setPosicion(String pos){
+        if(!pos.isEmpty){
+            posicion = pos;
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public int getDorsal(){
+        return dorsal;
+    }
+
+    public boolean setDorsal(int Dorsal){
+        if(!dorsal.isEmpty()){
+            this.dorsal = dorsal;
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
